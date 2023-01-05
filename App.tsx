2a5e1c4d15c5462a6ea452535b26d6e58cfa959e
RCurvo <renan.curvo@gmail.com>
2023-01-05 13:01:02 -0300

    feat: Implements Routes to getAuth

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from '@expo-google-fonts/roboto'
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading'
-import { SignUp } from '@screens/SignUp'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ RobotoRegular, RobotoBold })
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
