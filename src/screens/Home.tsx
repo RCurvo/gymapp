aa30610041e19023991dbbcae8c97d4a4c7c8f68
RCurvo <renan.curvo@gmail.com>
2023-01-07 10:23:53 -0300

    feat: implement Groups with flatlist

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,31 @@
+import { useState } from 'react'
+import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack, Text } from 'native-base'
+import { FlatList, HStack, VStack } from 'native-base'
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'Peito', 'pernas', 'Bíceps'])
+  const [groupSelected, setGroupSelected] = useState('costas')
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
