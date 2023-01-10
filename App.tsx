5083ef40277e867278f5c0bbc2dc95d1010505bc
RCurvo <renan.curvo@gmail.com>
2023-01-10 15:53:46 -0300

    feat: add AuthProvider component

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
-import { AuthContext } from '@contexts/AuthContext'
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ RobotoRegular, RobotoBold })
@@ -19,18 +19,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: '1',
-            name: 'Renan',
-            email: 'renan.curvo@gmail.com',
-            avatar: 'https://github.com/rcurvo.png',
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   )
 }
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { UserDTO } from '@dtos/UserDTO'
-import { createContext } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -8,3 +8,19 @@ export type AuthContextDataProps = {
 export const AuthContext = createContext<AuthContextDataProps>(
   {} as AuthContextDataProps,
 )
+
+type AuthContextProviderProps = {
+  children: ReactNode
+}
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Renan Curvo',
+    email: 'renan.curvo@gmail.com',
+    avatar: 'https://github.com/rcurvo.png',
+  })
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  )
+}
