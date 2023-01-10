2105e9e68c5a7d4e75d8a4bde6471330056f352f
RCurvo <renan.curvo@gmail.com>
2023-01-10 15:36:50 -0300

    feat: create context and UserDTO

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 import { THEME } from './src/theme'
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
+import { AuthContext } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ RobotoRegular, RobotoBold })
@@ -18,7 +19,18 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContext.Provider
+        value={{
+          user: {
+            id: '1',
+            name: 'Renan',
+            email: 'renan.curvo@gmail.com',
+            avatar: 'https://github.com/rcurvo.png',
+          },
+        }}
+      >
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
     </NativeBaseProvider>
   )
 }
