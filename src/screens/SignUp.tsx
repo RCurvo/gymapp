5ad5e09eaa6f9030cd98dfac464a6cf8f9a00584
RCurvo <renan.curvo@gmail.com>
2023-01-10 15:24:37 -0300

    lint: remove useless imports

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -17,8 +17,6 @@ import { useForm, Controller } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { api } from '@services/api'
-import axios from 'axios'
-import { Alert } from 'react-native'
 import { AppError } from '@utils/AppError'
 
 type FormDataProps = {
